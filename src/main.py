b3ddc224d757995e105d241b6f898bf02cb65818
Sinenzama10 <61007303+Sinenzama10@users.noreply.github.com>
2024-02-04 16:09:35 +0200

    Update main.py
    Included some error handling and a message at the end of the file's execution to inform that the process is complete

--- src/main.py
+++ src/main.py
@@ -6,18 +6,27 @@ from extract import get_repos, get_pull_requests
 from spark_process import transform_and_save
 
 def main():
-    repos = get_repos()
-    for repo in repos:
-        repo_name = repo.get('name')
-        owner = repo.get('owner').get('login')
-        prs = get_pull_requests(owner, repo_name)
+    try:
+        repos = get_repos()
+        for repo in repos:
+            repo_name = repo.get('name')
+            owner = repo.get('owner').get('login')
+            prs = get_pull_requests(owner, repo_name)
+            
+            # Assuming data directory exists
+            with open(f'../data/{repo_name}_prs.json', 'w') as f:
+                json.dump(prs, f)
         
-        # Assuming data directory exists
-        with open(f'../data/{repo_name}_prs.json', 'w') as f:
-            json.dump(prs, f)
+        # Assuming the JSON files are saved correctly in the data directory
+        transform_and_save('../data/*.json', '../output/prs_aggregated.parquet')
+        
+        # Print completion message
+        print("ETL pipeline has been run successfully.")
     
-    # Assuming the JSON files are saved correctly in the data directory
-    transform_and_save('../data/*.json', '../output/prs_aggregated.parquet')
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        # Log the fact that an error occured
+        raise
 
 if __name__ == "__main__":
     main()
