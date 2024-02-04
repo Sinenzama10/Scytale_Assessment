2617489bf22c9e04f58da9d7b1a30c57c9384e89
Sinenzama10 <61007303+Sinenzama10@users.noreply.github.com>
2024-02-04 17:09:31 +0200

    Update parquet_check.py
    To show organisation name as the first column

--- src/parquet_check.py
+++ src/parquet_check.py
@@ -1,12 +1,27 @@
-# parquet_check.py
 from pyspark.sql import SparkSession
 
 def check_parquet_file(parquet_file_path):
     spark = SparkSession.builder.appName("ParquetFileCheck").getOrCreate()
+    
+    # Read the Parquet file
     parquet_df = spark.read.parquet(parquet_file_path)
+    
+    # Reorder columns to have 'Organization Name' as the first column
+    all_columns = parquet_df.columns
+    if 'Organization Name' in all_columns:  # Check if 'Organization Name' is in the columns
+        reordered_columns = ['Organization Name'] + [column for column in all_columns if column != 'Organization Name']
+        parquet_df = parquet_df.select(*reordered_columns)
+    
+    # Show the DataFrame schema after reordering
     print("DataFrame Schema:")
     parquet_df.printSchema()
+    
+    # Show the first few rows of the DataFrame
     print("First few rows of the DataFrame:")
     parquet_df.show()
+    
+    # Print the row count
     print("Row count:", parquet_df.count())
+    
+    # Stop the Spark session
     spark.stop()
