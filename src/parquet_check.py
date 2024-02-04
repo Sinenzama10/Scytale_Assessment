1db4bb25ec9d92cd95328131ec60a061f086d9b0
Sinenzama10 <61007303+Sinenzama10@users.noreply.github.com>
2024-02-04 16:45:34 +0200

    Update parquet_check.py
    Added positional argument in order for it to accept

--- src/parquet_check.py
+++ src/parquet_check.py
@@ -1,29 +1,12 @@
-#Just a script to visually validate the output of the parquet files
+# parquet_check.py
 from pyspark.sql import SparkSession
 
-def check_parquet_file():
-    # Initialize Spark session
+def check_parquet_file(parquet_file_path):
     spark = SparkSession.builder.appName("ParquetFileCheck").getOrCreate()
-
-    # Path to your parquet file
-    parquet_file_path = '../output/prs_aggregated.parquet'
-
-    # Read the parquet file into a DataFrame
     parquet_df = spark.read.parquet(parquet_file_path)
-
-    # Show the DataFrame schema to verify structure
     print("DataFrame Schema:")
     parquet_df.printSchema()
-
-    # Show the first few rows of the DataFrame
     print("First few rows of the DataFrame:")
     parquet_df.show()
-
-    # If you just want to verify the row count
     print("Row count:", parquet_df.count())
-
-    # Stop the Spark session
     spark.stop()
-
-if __name__ == "__main__":
-    check_parquet_file()
